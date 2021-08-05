bce1702b1b3fec14579b5ebfe84044169e3930a1
tammerb <tammer.barkouki@gmail.com>
2021-08-05 14:24:43 -0700

    reorganize files into ros pkg

---
+++ src/explainBT_node.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "ros/ros.h"
+#include <behaviortree_cpp_v3/bt_factory.h>
+#include "explainBT_pkg/Explain.h"
+#include "ExplainableBT.h"
+using namespace std;
+
+
+int main (int argc, char **argv)
+{
+   ROS_INFO("Explainble BT server.");
+
+   return 0
+}
