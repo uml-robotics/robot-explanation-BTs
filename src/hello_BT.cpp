5a5ee5fbb529650b169dd5b01c3f8986adb1a7a3
tammerb <tammer.barkouki@gmail.com>
2021-08-09 21:58:52 -0700

    Rename pkg to conform to all lowercase, use abs. path for xml in hello_BT

--- src/hello_BT.cpp
+++ src/hello_BT.cpp
@@ -32,7 +32,7 @@ int main()
 
     // IMPORTANT: when the object "tree" goes out of scope, all the 
     // TreeNodes are destroyed
-    auto tree = factory.createTreeFromFile("./my_tree.xml");
+    auto tree = factory.createTreeFromFile("/home/tammer/explainBT/src/my_tree.xml");
 
     // To "execute" a Tree you need to "tick" it.
     // The tick is propagated to the children based on the logic of the tree.
