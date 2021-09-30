a8e4f283c4cff3cb22c40606f242dbb4ffd4549f
Tammer Barkouki <tammer.barkouki@gmail.com>
2021-09-29 21:06:12 -0700

    update to be a python 3 file

--- src/interface.py
+++ src/interface.py
@@ -1,14 +1,15 @@
 import os
 
-print "Test Explainable BT interface: \nEnter \"1\" for \"What are you doing?\"\nEnter \"2\" for \"Why are you doing this?\"\nEnter \"3\" for \"How do you achieve your goal?\" "
+print("Test Explainable BT interface: \nEnter \"1\" for \"What are you doing?\"\nEnter \"2\" for \"Why are you doing this?\"\nEnter \"3\" for \"How do you achieve your goal?\"")
 
 while True:
-    q = int(raw_input("Question type: "))
+    q = int(input("Question type: "))
     if q == 1:
         what = "What are you doing?"
     elif q == 2:
         what = "Why are you doing this?"
     elif q == 3:
         what = "How do you achieve your goal?"
-    print what
+    print(what)
     os.system("rosservice call /explainable_bt \"what: '" + what + "'\"")
+
